2eace4acbcb70d26980299bf8031ea4bf2ebfb36
saichethanr <saichethan9019@gmail.com>
2024-08-16 19:56:14 +0530

    sent a html responce using the .getwriter.println command

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,10 @@ import org.apache.catalina.startup.Tomcat;
 public class App 
 {
     public static void main( String[] args ) throws LifecycleException {
-        System.out.println( "Hello World!" );
+
         Tomcat tomcat = new Tomcat();
         tomcat.start();
+        tomcat.setPort(8080);
         tomcat.getServer().await();
 
         Context context = tomcat.addContext("",null);
--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -5,11 +5,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 
 //if u want to activate a servlet we have to send a request to the servlet
-
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("in service");
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>hello world</h1>");
     }
 }
